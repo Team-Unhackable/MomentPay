3c80a3f15309fac54228b8f85d9633d29d0ae465
David Jiang <jiangguanlin2003@outlook.com>
2023-06-15 09:44:26 +0200

    Update Login.js & Change some style in Login Page

--- splitpay-frontend/src/pages/Login.js
+++ splitpay-frontend/src/pages/Login.js
@@ -1,50 +1,3 @@
-// import '../App.css';
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-
-
-
-// function Login() {
-//   const navigate = useNavigate();
-
-//   function auth() {
-//     var username = document.getElementById("username").value;
-//     var password = document.getElementById("password").value;
-//     // var data = {username: username, password: password};
-//     fetch('https://my-json-server.typicode.com/Team-Unhackable/MomentPay-Demo-Data/' + username)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//       if (data.password === password) {
-//         localStorage.setItem('login', true);
-//         navigate('/');
-//       } else {
-//         return false;
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     });
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Login</h1>
-//         <form action="/login" method="post">
-//             <label for="username" className='formLabel'>Username: </label>
-//             <input type="text" id="username" name="username" required className='form'></input><br></br><br></br>
-//             <label for="password" className='formLabel'>Password: </label>
-//             <input type="password" id="password" name="password" required className='form'></input><br></br><br></br>
-//             <input type="submit" value="Submit" className='loginButton' onClick={auth}></input>
-//         </form>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -82,20 +35,20 @@ function Login({Login, error}) {
         <form onSubmit={submitHandler}>
             <div className="form-inner">
                 <center>
-                <h2>Account Login</h2>
+                <h1>Account Login</h1>
                 <br></br>
                 {(error !== "") ? ( <div className="error">{error}</div> ) : ""}
                 <div className="form-group">
-                    <label htmlFor="name">User Name: </label>
-                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value = {details.name} />
+                    <label htmlFor="name" className='formLabel'>User Name: </label>
+                    <input type="text" name="name" id="name" className='form' onChange={e => setDetails({...details, name: e.target.value})} value = {details.name} />
                 </div>
                 <br></br>
                 <div className="form-group">
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value = {details.password} />
+                    <label htmlFor="password" className='formLabel'>Password: </label>
+                    <input type="password" name="password" id="password" className='form' onChange={e => setDetails({...details, password: e.target.value})} value = {details.password} />
                 </div>
                 <br></br>
-                <input type="submit" value="LOGIN" />
+                <input type="submit" className='loginButton' value="LOGIN" />
                 </center>
             </div>
         </form>
