cc725de82a0699fe5531a9eca4f071608687a10b
David Jiang <jiangguanlin2003@outlook.com>
2023-06-16 07:18:21 +0200

    Update: Add Expense & Fix some bugs, Expense CSS part have some problems

--- splitpay-frontend/src/pages/Expense.js
+++ splitpay-frontend/src/pages/Expense.js
@@ -1,9 +1,46 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+import Owns_You from '../components/Owns_You';
+import Paid from '../components/Paid';
+import Total from '../components/Total';
+import ExpenseList from '../components/ExpenseList';
+
+
+
 const Expense = () => {
     return (
-        <center>
-        <h1>Expense</h1>
-        </center>
-    )
+        <div className='container'>
+        <h1 className='mt-3'>Account</h1>
+        <div className='row mt-3'>
+            <div className='col-sm'>
+                <Total/>
+            </div>
+
+            <div className='col-sm'>
+                <Owns_You />
+            </div>
+
+            <div className='col-sm'>
+                <Paid />
+            </div>
+
+            </div>
+			<h3 className='mt-3'>Expenses</h3>
+			<div className='row mt-3'>
+				<div className='col-sm'>
+					<ExpenseList />
+				</div>
+
+                </div>
+			<h3 className='mt-3'>Add Expense</h3>
+			<div className='row mt-3'>
+				<div className='col-sm'>
+					{/* <AddExpenseForm /> */}
+				</div>
+                
+        </div>
+    </div>
+);
 };
 
-export default Expense;
+export default Expense
