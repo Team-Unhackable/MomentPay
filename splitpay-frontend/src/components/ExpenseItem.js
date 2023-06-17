450ae79103de179161b514abc6fbc705ef4151d6
David Jiang <jiangguanlin2003@outlook.com>
2023-06-17 08:14:24 +0200

    Update Expense API fetch & Expense Page

--- splitpay-frontend/src/components/ExpenseItem.js
+++ splitpay-frontend/src/components/ExpenseItem.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { useNavigate } from 'react-router-dom';
 
 const ExpenseItem = (props) => {
+	const navigate = useNavigate();
+	function handlePay() {
+		if (localStorage.getItem('paymentStatus') === 'true') {
+			alert('You have already paid!');
+		} else {
+			navigate('/payment');
+		}
+	}
+	
 	return (
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
-			{props.name}
+			<h3>{props.name}</h3>
 			<div>
 				<span className='badge badge-primary badge-pill mr-3'>
-					£{props.cost}
+					<h3>{props.total_cost} $HKD</h3>
 				</span>
+				<span><button onClick={handlePay} className='handlePayButton'> Pay </button></span>
 				<TiDelete size='1.5em'></TiDelete>
 			</div>
 		</li>
--- splitpay-frontend/src/components/ExpenseList.js
+++ splitpay-frontend/src/components/ExpenseList.js
@@ -2,21 +2,19 @@ import React from 'react'
 import ExpenseItem from './ExpenseItem';
 
 const ExpenseList = () => {
+    console.log(localStorage.getItem("paymentPlace"))
+    console.log(localStorage.getItem("totalAmount"))
     const Expenses = [
-        {id: 1,Event_name: '',Total_cost:100,},
-        { id: 13, Event_name: 'holiday', total_cost: 800, },
-		{ id: 14, Event_name: 'car service', total_cost: 100, },
-        //friendsplit:{frirend_name: Alice, fee:500}
+        {id: 1,Event_name: localStorage.getItem("paymentPlace"),Total_cost: localStorage.getItem("totalAmount")},
     ]
 
+    return(
+        <ul className='list-group'>
+            {Expenses.map((Expense) => (
+                <ExpenseItem id={Expense.id} name={Expense.Event_name} total_cost={Expense.Total_cost}   />
+            ))}
+        </ul>
+    )
+}
 
-        return(
-            <ul className='list-group'>
-                {Expenses.map((Expense) => (
-                    <ExpenseItem id={Expense.id} name={Expense.name} total_cost={Expense.total_cost}   />
-                 ))}
-                </ul>
-        )
-    }
-
-        export default ExpenseList
+export default ExpenseList
